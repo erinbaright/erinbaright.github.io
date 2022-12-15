f2cf3e10f409290b9623ba268387ab7270e29214
erinbaright <erin.baright29@gmail.com>
2022-12-14 21:50:00 -0500

    push

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,9 +3,6 @@ import './portfolio.css'
 import IMG1 from '../../assets/breddit.png'
 import IMG2 from '../../assets/petlyfe.png'
 import IMG3 from '../../assets/guessthecity.png'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
 
 const data = [
   {
